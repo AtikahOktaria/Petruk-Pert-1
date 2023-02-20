d1210a22686c4626e9f37a4fea6c3092360f35c6
Atikah Oktaria <112566355+AtikahOktaria@users.noreply.github.com>
2023-02-20 16:41:57 +0700

    Add files via upload

---
+++ Introduce.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+	string a, b, c;
+	getline(cin,a);
+	cin>>b;
+	cin>>c;
+	cout<< "Izin memperkenalkan diri nama saya "<<a<<" dengan jumlah huruf 24. Saya mahasiswa S1 Ilmu komputer dari kelas "<<b<< ".Nilai DDP saya adalah "<<c;
+	return 0;
+}
---
+++ Lingkaran.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+	float a, phi, L;
+	phi=3.14;
+	cin>>a;
+	L=phi*a*a;
+	cout<<L;
+	return 0;
+}
